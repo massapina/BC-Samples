6c240b50b614fc952ca5f015bdfa3fd1209ac66b
SBND Team <sdimov@sbnd.net>
2014-11-12 09:40:10 +0200

    mobile-web-api
    candidate release at 12/11/2014

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>NetSwipe Demostration Page</title>
+		<link href="css/styles.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<h1><img src="../img/logo.png" alt="NetSwipe">Demonstration Page</h1>
+		</header>
+		<article class="product">
+			<header>
+				<h2>Jumio T-Shirt</h2>
+			</header>
+			<section>
+				<div class="image">
+					<img src="img/shirts.png" alt="Product">
+					<i>New</i>
+				</div>
+				<div class="info">
+					<h3>
+						<strong>Brand: Jumio</strong>
+						<br>
+						<i>Price: $9.99</i>
+					</h3>
+					<p>
+						Size: Unifit<br>
+						Color: Navy
+					</p>
+					<p>Description: Lorem ipsum dolor sit amet, consectetur elit. Suspendisse ut lacus sed ante rhoncus consectetur ut vel risus. Integer id porttitor erat. Quisque fringilla dui ut tempus scelerisque. Proin auctor libero sed scelerisque lobortis.</p>
+					<a href="form.php" class="btn">Buy with credit card &raquo;</a>
+				</div>
+			</section>
+		</article>
+		<footer>Supported by <a href="https://www.jumio.com/">Jumio</a></footer>
+	</body>
+</html>
